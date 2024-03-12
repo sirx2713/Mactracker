6161b78ca1cb5dd8ba02e4d0cf3bbfd43564aa18
sirx2713 <bradx2713@gmail.com>
2024-03-13 00:36:45 +0300

    Initial Commit

---
+++ Mactracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mactracker
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mactracker/MactrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MactrackerApp.swift
+//  Mactracker
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/13.
+//
+
+import SwiftUI
+
+@main
+struct MactrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
